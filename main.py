73ca444c165f04c4d2222150e25cd964f0dc82d2
Pintu <87765219+PintuDebNath@users.noreply.github.com>
2022-12-08 16:27:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def armstrong(n):
     # }
     return jsonify(alldata)
 if __name__ == "__main__":
-    app.run()
+    app.run(debug="False",host('0.0.0.0'))
     # Debug/Development
     # app.run(debug=True, host="0.0.0.0", port="5000")
     # Production
